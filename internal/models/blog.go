545ff9f619f91bcb2ad8935b89d8caecf0cc108f
Iretoms <iretomiwajonathan@gmail.com>
2024-07-21 08:28:11 +0100

    blog model

--- internal/models/blog.go
+++ internal/models/blog.go
@@ -1,8 +1,15 @@
 package models
 
+import (
+	"time"
+)
+
 type Blog struct {
-	ID      string `gorm:"primaryKey"`
-	Title   string
-	Content string
-	Author  string
+	ID        string    `gorm:"type:uuid;primary_key" json:"id"`
+	Title     string    `gorm:"not null" json:"title"`
+	Content   string    `gorm:"type:text" json:"content"`
+	AuthorID  string    `gorm:"type:uuid;not null" json:"author_id"`
+	Author    User      `gorm:"foreignKey:AuthorID" json:"author,omitempty"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
