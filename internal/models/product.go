da2996db894543c5b75937f503a6ee9c6ef990ad
urizennnn <igamerryt@gmail.com>
2024-07-24 23:17:54 +0100

    chore:added delete product

--- internal/models/product.go
+++ internal/models/product.go
@@ -1,12 +1,56 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"gorm.io/gorm"
+
+	"github.com/hngprojects/hng_boilerplate_golang_web/pkg/repository/storage/postgresql"
+)
 
 type Product struct {
-	ID          string    `gorm:"type:uuid;primaryKey" json:"product_id"`
-	Name        string    `gorm:"column:name; type:varchar(255); not null" json:"name"`
-	Description string    `gorm:"column:description;type:text;" json:"description"`
-	OwnerID      string    `gorm:"type:uuid;" json:"owner_id"`
-	CreatedAt   time.Time `gorm:"column:created_at; not null; autoCreateTime" json:"created_at"`
-	UpdatedAt   time.Time `gorm:"column:updated_at; null; autoUpdateTime" json:"updated_at"`
+	ID          string     `gorm:"type:uuid;primaryKey" json:"product_id"`
+	Name        string     `gorm:"column:name; type:varchar(255); not null" json:"name"`
+	Price       float64    `gorm:"column:price; type:decimal(10,2);not null" json:"price"`
+	Description string     `gorm:"column:description; type:text" json:"description"`
+	OwnerID     string     `gorm:"type:uuid;" json:"owner_id"`
+	Category    []Category `gorm:"many2many:product_categories;;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;" json:"category"`
+	CreatedAt   time.Time  `gorm:"column:created_at; not null; autoCreateTime" json:"created_at"`
+	UpdatedAt   time.Time  `gorm:"column:updated_at; null; autoUpdateTime" json:"updated_at"`
+}
+
+type CreateProductRequestModel struct {
+	Name        string  `json:"name" validate:"required"`
+	Description string  `json:"description" validate:"required"`
+	Price       float64 `json:"price" validate:"required"`
+}
+
+type DeleteProductRequestModel struct {
+	ProductID string `json:"product_id" validate:"required"`
+}
+
+func (u *Product) CreateProduct(db *gorm.DB) error {
+	err := postgresql.CreateOneRecord(db, &u)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (p *Product) AddProductToCategory(db *gorm.DB, categories []interface{}) error {
+	// Add product to categories
+	err := db.Model(p).Association("Categories").Append(categories...)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (p *Product) DeleteProduct(db *gorm.DB) error {
+	err := postgresql.DeleteRecordFromDb(db, p)
+	if err != nil {
+		return err
+	}
+	return nil
 }
--- internal/models/seed/seed.go
+++ internal/models/seed/seed.go
@@ -1,3 +1,4 @@
+
 package seed
 
 import (
@@ -11,13 +12,21 @@ import (
 )
 
 func SeedDatabase(db *gorm.DB) {
-	// instantiate uuid
-
+	// Create UUIDs for users
 	Userid1 := utility.GenerateUUID()
+	Userid2 := utility.GenerateUUID()
+
+	categories := []models.Category{
+		{ID: utility.GenerateUUID(), Name: "Fashion"},
+		{ID: utility.GenerateUUID(), Name: "Groceries"},
+		{ID: utility.GenerateUUID(), Name: "Appliances"},
+	}
+
+	// Create users
 	user1 := models.User{
-		ID: Userid1,
-		Name:   "John Doe",
-		Email:  "john@example.com",
+		ID:       Userid1,
+		Name:     "John Doe",
+		Email:    "john@example.com",
 		Password: utility.RandomString(20),
 		Profile: models.Profile{
 			ID:        utility.GenerateUUID(),
@@ -27,17 +36,16 @@ func SeedDatabase(db *gorm.DB) {
 			AvatarURL: "http://example.com/avatar.jpg",
 		},
 		Products: []models.Product{
-			{ID: utility.GenerateUUID(), Name: "Product1", Description: "Description1", OwnerID: Userid1},
-			{ID: utility.GenerateUUID(), Name: "Product2", Description: "Description2", OwnerID: Userid1},
+			{ID: utility.GenerateUUID(), Name: "Product1", Description: "Description1", Price: 45.33, OwnerID: Userid1},
+			{ID: utility.GenerateUUID(), Name: "Product2", Description: "Description2", Price: 45.33, OwnerID: Userid1},
 		},
 	}
 
-	Userid2 := utility.GenerateUUID()
 	user2 := models.User{
-		ID: Userid2,
-		Name:   "Jane Doe",
+		ID:       Userid2,
+		Name:     "Jane Doe",
+		Email:    "jane@example.com",
 		Password: utility.RandomString(20),
-		Email:  "jane@example.com",
 		Profile: models.Profile{
 			ID:        utility.GenerateUUID(),
 			FirstName: "Jane",
@@ -46,37 +54,57 @@ func SeedDatabase(db *gorm.DB) {
 			AvatarURL: "http://example.com/avatar2.jpg",
 		},
 		Products: []models.Product{
-			{ID: utility.GenerateUUID(), Name: "Product3", Description: "Description3", OwnerID: Userid2},
-			{ID: utility.GenerateUUID(), Name: "Product4", Description: "Description4", OwnerID: Userid2},
+			{ID: utility.GenerateUUID(), Name: "Product3", Description: "Description3", Price: 45.33, OwnerID: Userid2},
+			{ID: utility.GenerateUUID(), Name: "Product4", Description: "Description4", Price: 45.33, OwnerID: Userid2},
 		},
 	}
 
+	// Create organisations and categories
 	organisations := []models.Organisation{
-		{ID: utility.GenerateUUID(), Name: "Org1", Email: fmt.Sprintf(utility.RandomString(4)+"@email.com"),Description: "Description1", OwnerID: Userid1},
-		{ID: utility.GenerateUUID(), Name: "Org2", Email: fmt.Sprintf(utility.RandomString(4)+"@email.com"),Description: "Description2", OwnerID: Userid1},
-		{ID: utility.GenerateUUID(), Name: "Org3", Email: fmt.Sprintf(utility.RandomString(4)+"@email.com"),Description: "Description3", OwnerID: Userid2},
+		{ID: utility.GenerateUUID(), Name: "Org1", Email: fmt.Sprintf("%s@email.com", utility.RandomString(4)), Description: "Description1", OwnerID: Userid1},
+		{ID: utility.GenerateUUID(), Name: "Org2", Email: fmt.Sprintf("%s@email.com", utility.RandomString(4)), Description: "Description2", OwnerID: Userid1},
+		{ID: utility.GenerateUUID(), Name: "Org3", Email: fmt.Sprintf("%s@email.com", utility.RandomString(4)), Description: "Description3", OwnerID: Userid2},
 	}
 
 	var existingUser models.User
-	if err := db.Preload("Profile").Preload("Products").Where("email = ?", user1.Email).First(&existingUser).Error; err != nil {
+
+	if err := db.Preload("Profile").Preload("Products").Preload("Products.Categories").Where("email = ?", user1.Email).First(&existingUser).Error; err != nil {
 		if err == gorm.ErrRecordNotFound {
+			// Seed users
 			postgresql.CreateOneRecord(db, &user1)
 			postgresql.CreateOneRecord(db, &user2)
+
+			// Seed organisations
 			for _, org := range organisations {
 				postgresql.CreateOneRecord(db, &org)
 			}
-			fmt.Println("Users and organisations seeded.")
 
-			// Add users to organisations
+			// Seed categories
+			for _, category := range categories {
+				postgresql.CreateOneRecord(db, &category)
+			}
+
+			fmt.Println("Users, organisations and categories seeded.")
+
+			// // Add users to organisations
 			existingUser.AddUserToOrganisation(db, &user1, []interface{}{&organisations[0], &organisations[1]})
-			existingUser.AddUserToOrganisation(db, &user2, []interface{}{&organisations[0], &organisations[1], &organisations[2]})
+			existingUser.AddUserToOrganisation(db, &user2, []interface{}{&organisations[0], &organisations[1]})
 			fmt.Println("Users added to organisations.")
 
+			// Add products to categories
+			for _, product := range user1.Products {
+				product.AddProductToCategory(db, []interface{}{&categories[0], &categories[1]})
+			}
+
+			for _, product := range user2.Products {
+				product.AddProductToCategory(db, []interface{}{&categories[0], &categories[1]})
+			}
+
+			fmt.Println("Products added to categories.")
 		} else {
 			fmt.Println("An error occurred: ", err)
 		}
 	} else {
 		fmt.Println("Users already exist, skipping seeding.")
 	}
-
 }
