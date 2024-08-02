afc2e5c2f0872161fc02aef8a7624cc0fe5c7d8d
Osinachi Chukwujama <osinachi.chukwujama@gmail.com>
2024-08-02 11:45:32 +0100

    chore: remove comments

--- pkg/middleware/metrics.go
+++ pkg/middleware/metrics.go
@@ -20,11 +20,9 @@ var (
 )
 
 func init() {
-	// Register the custom metric with the default registry
 	prometheus.MustRegister(httpRequestDuration)
 }
 
-// Metrics is a middleware function for tracking HTTP request durations
 func Metrics(cfg *config.Configuration) gin.HandlerFunc {
 	appName := cfg.App.Name
 	if appName == "" {
@@ -33,19 +31,14 @@ func Metrics(cfg *config.Configuration) gin.HandlerFunc {
 	}
 
 	return func(c *gin.Context) {
-		// Start a timer to track request duration
 		startTime := time.Now()
 
-		// Get the request method
 		method := c.Request.Method
 
-		// Allow the request to be processed by calling the next handler
 		c.Next()
 
-		// Get the response status code after the request is processed
 		statusCode := c.Writer.Status()
 
-		// Calculate and record the request duration
 		elapsed := time.Since(startTime).Seconds()
 		httpRequestDuration.WithLabelValues(method, c.FullPath(), strconv.Itoa(statusCode), appName).Observe(elapsed)
 	}
