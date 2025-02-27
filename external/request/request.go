a38f533482aa9dca085901d192d9297b198665f7
Emmanuel Emonueje Ebenezer <eebenezer949@gmail.com>
2025-02-27 14:32:21 +0100

    [refactor]: define external implementation for SendExternalRequest interface

--- external/request/request.go
+++ external/request/request.go
@@ -1,51 +1,87 @@
 package request
 
 import (
-	"fmt"
+	"errors"
 
+	"github.com/hngprojects/hng_boilerplate_golang_web/external"
+	"github.com/hngprojects/hng_boilerplate_golang_web/external/ipstack"
 	"github.com/hngprojects/hng_boilerplate_golang_web/external/mocks"
-	"github.com/hngprojects/hng_boilerplate_golang_web/external/thirdparty/ipstack"
-	"github.com/hngprojects/hng_boilerplate_golang_web/internal/config"
 	"github.com/hngprojects/hng_boilerplate_golang_web/utility"
 )
 
+// ExternalRequest holds fields for communicating with external services
+//
+// Create a new ExternalRequest using NewExternalRequest,
+// pass in logger and request config options(WithTest, WithIpstack) to configure ExternalRequest
+//
+// Call SetupExternalRequest before SendRequest to send requests to external-services
 type ExternalRequest struct {
-	Logger *utility.Logger
-	Test   bool
+	Logger      *utility.Logger
+	Test        bool
+	Ipstack     *ipstack.IpstackRequest
+	requestName string
 }
 
-var (
-	// serializers
-	JsonDecodeMethod    string = "json"
-	PhpSerializerMethod string = "phpserializer"
+type sendExtReqOpts func(*ExternalRequest) *ExternalRequest
 
-	// requests
-	IpstackResolveIp string = "ipstack_resolve_ip"
+var (
+	ErrRequestNotFound = errors.New("request not found")
 )
 
-func (er ExternalRequest) SendExternalRequest(name string, data interface{}) (interface{}, error) {
-	var (
-		config = config.GetConfig()
-	)
-	if !er.Test {
-		switch name {
-		case IpstackResolveIp:
-			obj := ipstack.RequestObj{
-				Name:         name,
-				Path:         fmt.Sprintf("%v", config.IPStack.BaseUrl),
-				Method:       "GET",
-				SuccessCode:  200,
-				DecodeMethod: JsonDecodeMethod,
-				RequestData:  data,
-				Logger:       er.Logger,
-			}
-			return obj.IpstackResolveIp()
+func WithTest(test bool) sendExtReqOpts {
+	return func(sr *ExternalRequest) *ExternalRequest {
+		sr.Test = test
+		return sr
+	}
+}
+
+func WithIPstack(ir *ipstack.IpstackRequest) sendExtReqOpts {
+	return func(sr *ExternalRequest) *ExternalRequest {
+		sr.Ipstack = ir
+		sr.Ipstack.Logger = sr.Logger
+		return sr
+	}
+}
+
+func NewExternalRequest(logger *utility.Logger, opts ...sendExtReqOpts) *ExternalRequest {
+	sendExtRequest := ExternalRequest{
+		Logger: logger,
+	}
+
+	for _, op := range opts {
+		op(&sendExtRequest)
+	}
+	return &sendExtRequest
+}
+
+func (sr *ExternalRequest) SetupExternalRequest(name string, data any) error {
+	switch name {
+	case external.IpstackResolveIp:
+		err := sr.Ipstack.SetupExternalRequest(name, data)
+		if err != nil {
+			return err
+		}
+	default:
+		return ErrRequestNotFound
+	}
+	return nil
+}
+func (sr *ExternalRequest) SendRequest() (any, error) {
+	if !sr.Test {
+		switch sr.requestName {
+		case external.IpstackResolveIp:
+			return sr.Ipstack.SendRequest()
 		default:
-			return nil, fmt.Errorf("request not found")
+			return nil, ErrRequestNotFound
+		}
+	} else {
+		switch sr.requestName {
+		case external.IpstackResolveIp:
+			mer := mocks.ExternalRequest{Logger: sr.Logger, Test: true}
+			resp, err := mer.SendExternalRequest(sr.requestName, sr.Ipstack.RequestData)
+			return resp, err
 		}
 
-	} else {
-		mer := mocks.ExternalRequest{Logger: er.Logger, Test: true}
-		return mer.SendExternalRequest(name, data)
 	}
+	return nil, nil
 }
